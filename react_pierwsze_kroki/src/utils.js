0680f2ddf72fbb3f6b6cb7ece882a45bdf19782e
tester <berera@p2.pl>
2024-01-09 20:10:40 +0100

    fixed hour valide number

--- react_pierwsze_kroki/src/utils.js
+++ react_pierwsze_kroki/src/utils.js
@@ -1,5 +1,9 @@
 export function isValidNumberInput(e) {
-    if(isNaN(parseInt(e.key, 10))===true)
-    return e.preventDefault();
-    return true;
+	if (
+		isNaN(parseInt(e.key, 10)) === false ||
+		e.key === "Backspace" ||
+		e.key === "Delete"
+	)
+		return true;
+	return e.preventDefault();
 }
